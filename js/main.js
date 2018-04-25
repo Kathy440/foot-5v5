94077f15fa20d7e7b56947b220e705eb17cf189c
VOISIN Kathy <mkathy.voisin@gmail.com>
2018-04-25 17:04:39 +0200

    Test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+});
+
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: false,
+    stopAutoOnClick:false,
+    pager: false,
+});
